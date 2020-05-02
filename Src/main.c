159d44b05f98341d00881f11c88811777ba2bb01
Supreeth <supreethanil128@gmail.com>
2020-05-02 13:03:33 +0530

    Enable IYM for stm32f446re(Nucleo)

--- Src/main.c
+++ Src/main.c
@@ -16,12 +16,10 @@
  *
  ******************************************************************************
  */
-
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+#include <stdio.h>
 
 int main(void)
 {
+	printf("hahahahahahahahahahaha..........\n");
 	for(;;);
 }
